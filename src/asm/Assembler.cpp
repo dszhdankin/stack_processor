893a2c731d4b26e1a6714478558e611eab024428
Daniil Zhdankin <dszhdankin@edu.hse.ru>
2021-01-22 21:38:40 +0300

    Instruction classes implemented

---
+++ src/asm/Assembler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dszhdankin on 22.01.2021.
+//
+
+#include "Assembler.h"
---
+++ src/asm/Assembler.h
@@ -0,0 +1,14 @@
+//
+// Created by dszhdankin on 22.01.2021.
+//
+
+#ifndef STACK_PROCESSOR_ASSEMBLER_H
+#define STACK_PROCESSOR_ASSEMBLER_H
+
+
+class Assembler {
+
+};
+
+
+#endif //STACK_PROCESSOR_ASSEMBLER_H
